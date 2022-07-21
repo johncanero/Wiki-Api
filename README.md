ad1d69b6d05de023a4386306d8c5ff45bf1d0a74
John Canero <johncanero.official@gmail.com>
2022-07-22 00:18:57 +0800

    First Commit: Add to Github

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a Wiki-Api Project ⚪: Building your Own RESTful API guided by Angela Yu in her Web Development Bootcamp.
+
+Project by John Cañero
---
+++ app.js
@@ -0,0 +1,37 @@
+//jshint esversion:6
+
+// essentials
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+// MONGO CLIENT
+const MongoClient = require('mongodb').MongoClient
+
+
+// essentials: express
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+// MONGOOSE CONNECT
+mongoose.connect('mongodb://localhost:27017/', {
+    dbName: wikiDB,
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}, err => err ? console.log(err) : console.log('Connected to database'));
+
+// MONGOOSE - ITEMS SCHEMA
+
+
+
+//TODO
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
